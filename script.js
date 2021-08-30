129f4031e2b822ed77528488c8b1fba55969677c
Jessica Choe <jessj.choe@gmail.com>
2021-08-31 02:10:32 +0900

    create flashcard function

--- script.js
+++ script.js
@@ -28,8 +28,7 @@ async function getDictEntry(word) {
         if (typeof res.data[0] === 'string') {
             append(`did you mean`,res.data[0])
         } else {
-            let definition = res.data[0].shortdef[0];
-            let pos = res.data[0].fl;
+            
             let audio = res.data[0].hwi.prs[0].sound;
             let audioName = audio.audio;
 
@@ -46,19 +45,16 @@ async function getDictEntry(word) {
                 audioSub = audioName[0];
             }
 
-            audioURL = `${audioBaseURL}${audioSub}/${audioName}.mp3`;
+            let def = res.data[0].shortdef[0];
+            let pos = res.data[0].fl;
+            let audioURL = `${audioBaseURL}${audioSub}/${audioName}.mp3`;
+            let wordArray = [def, pos, audioURL];
+            append('part of speech', wordArray[1]);
+            append('definition', wordArray[0]);
             
-            append('part of speech', pos);
-            append('definition', definition);
-
-            
-
-            return {
-                definition,
-                pos,
-                audioURL,
-            }
+            return wordArray;
         }
+        
     } catch (error) {
         console.log(error);
     }
@@ -102,7 +98,7 @@ async function getDictEntry(word) {
 const content = document.querySelector('#content');
 
 function append(keyword, data) {
-    contentDiv.innerHTML = '';
+    
     let newContent = document.createElement('p');
     newContent.innerText = `${keyword}: ${data}`;
     contentDiv.appendChild(newContent);
@@ -116,14 +112,17 @@ function append(keyword, data) {
 
 const searchInput = document.createElement('input');
 const searchDiv = document.querySelector('#search-dictionary');
+
 function launchLearnWords(event) {
-    
+    contentDiv.innerHTML = '';
     contentName.innerHTML = '<h2>Learn Words</h2>';
     searchDiv.innerHTML = '<p>Search for words to learn their part of speech, meaning, and synonyms</p>'
 
     // searchInput = document.createElement('input');
     let searchBtn = document.createElement('button');
     searchBtn.classList.add('search-button');
+    searchBtn.innerText = 'search';
+    searchInput.placeholder = 'enter a word';
     searchDiv.append(searchInput);
     searchDiv.append(searchBtn);
 
@@ -134,8 +133,12 @@ function launchLearnWords(event) {
 // search dictionary upon input value
 function searchDict(event) {
     event.preventDefault;
+    contentDiv.innerHTML = '';
     const inputValue = searchInput.value;
-    getDictEntry(inputValue);
+    
+    let wordArr = getDictEntry(inputValue);
+    
+    
     searchInput.value = '';
 }
 
@@ -144,12 +147,19 @@ function playAudio(){
     audio.play();                // play it till it ends
 }
 
+const cardArray = [];
 function launchFlashcards() {
-    // contentName.innerHTML = '<h2>Flashcards</h2>';
-    // searchDiv.innerHTML = '<p>Flip the flashcard to see the meaning</p>'
+    contentDiv.innerHTML = '';
+    contentName.innerHTML = '<h2>Flashcards</h2>';
+    searchDiv.innerHTML = '<p>enter up to 10 words to create your deck of flashcards</p>'
+    const textInput = document.createElement('input');
+    const addBtn = document.createElement('button');
+    addBtn.classList.add('search-button');
+    addBtn.innerText = 'add';
+    textInput.placeholder = 'enter a word';
+    searchDiv.append(textInput);
+    searchDiv.append(addBtn);
 
-    // // searchInput = document.createElement('input');
-    // let cardBtn = document.createElement('button');
     // cardBtn.classList.add('card-button');
     // searchDiv.append(searchInput);
     // searchDiv.append(searchBtn);
@@ -165,6 +175,9 @@ function launchSpellingBee() {
 
 }
 
+function launchWotd() {
+    getDictEntry('hello');
+}
 // const button = document.querySelector('#pronounce');
 // button.addEventListener('click', playAudio(wordAudio))
 
